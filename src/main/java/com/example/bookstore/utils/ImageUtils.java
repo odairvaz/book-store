4d6b8af356a76a064adea756740931118d9b8b5f
Oda <odairvaz>
2023-09-13 10:51:40 +0200

    remove comment

--- src/main/java/com/example/bookstore/utils/ImageUtils.java
+++ src/main/java/com/example/bookstore/utils/ImageUtils.java
@@ -1,10 +1,3 @@
-/*
- * -----------------------------------------------------------------------------
- * © Swisslog AG
- * Swisslog is not liable for any usage of this source code that is not agreed on between Swisslog and the other party.
- * The mandatory legal liability remains unaffected.
- * -----------------------------------------------------------------------------
- */
 package com.example.bookstore.utils;
 
 import org.springframework.http.HttpStatus;
