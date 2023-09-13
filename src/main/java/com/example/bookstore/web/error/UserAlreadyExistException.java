c49466fc99c50e8fe9053c75bd42b8ef5033e389
Oda <odairvaz>
2023-09-13 19:39:21 +0200

    add serial anotation

--- src/main/java/com/example/bookstore/web/error/UserAlreadyExistException.java
+++ src/main/java/com/example/bookstore/web/error/UserAlreadyExistException.java
@@ -1,7 +1,10 @@
 package com.example.bookstore.web.error;
 
+import java.io.Serial;
+
 public final class UserAlreadyExistException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 5861310537366287163L;
 
     public UserAlreadyExistException() {
