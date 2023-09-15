9748c20b960a498479372f90b580f64f24445d4b
Odair Vaz <odair.vaz@swisslog.com>
2023-09-15 17:30:41 +0200

    create verification token entity

---
+++ src/main/java/com/example/bookstore/persistense/model/VerificationToken.java
@@ -0,0 +1,67 @@
+package com.example.bookstore.persistense.model;
+
+import jakarta.persistence.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+public class VerificationToken {
+
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private Date expiryDate;
+
+    public VerificationToken() {}
+
+    public VerificationToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    private Date calculateExpiryDate(final int expiryTimeInMinutes) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+}
