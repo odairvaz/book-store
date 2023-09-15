1cf23685cf0f90e8f10dd65171971b69c5145e9f
Odair Vaz <odair.vaz@swisslog.com>
2023-09-15 17:32:18 +0200

    confirm registration and trigger events.

--- src/main/java/com/example/bookstore/web/controller/RegistrationController.java
+++ src/main/java/com/example/bookstore/web/controller/RegistrationController.java
@@ -1,17 +1,24 @@
 package com.example.bookstore.web.controller;
 
 
+import com.example.bookstore.persistense.model.User;
+import com.example.bookstore.persistense.model.VerificationToken;
+import com.example.bookstore.registration.OnRegistrationCompleteEvent;
 import com.example.bookstore.service.IUserService;
 import com.example.bookstore.web.dto.UserDto;
 import com.example.bookstore.web.error.UserAlreadyExistException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/api/v1/user")
@@ -19,9 +26,13 @@ public class RegistrationController {
 
     public static final String REGISTRATION_PAGE = "registration";
     private final IUserService userService;
+    private final ApplicationEventPublisher eventPublisher;
+    private final MessageSource messages;
 
-    public RegistrationController(IUserService userService) {
+    public RegistrationController(IUserService userService, ApplicationEventPublisher eventPublisher, MessageSource messages) {
         this.userService = userService;
+        this.eventPublisher = eventPublisher;
+        this.messages = messages;
     }
 
     @GetMapping("/registration")
@@ -32,17 +43,44 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String registerUserAccount(@ModelAttribute("user") @Valid UserDto userDto, BindingResult bindingResult, Model model) {
+    public String registerUserAccount(@ModelAttribute("user") @Valid UserDto userDto, HttpServletRequest request, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return REGISTRATION_PAGE;
         }
 
         try {
-            userService.registerNewUserAccount(userDto);
+            User newUserAccount = userService.registerNewUserAccount(userDto);
+            String appUrl = request.getContextPath();
+            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(newUserAccount, request.getLocale(), appUrl));
         } catch (UserAlreadyExistException uaeEx) {
             bindingResult.rejectValue("email", "error.user", "An account with the email already exists.");
             return REGISTRATION_PAGE;
         }
         return "successRegister";
     }
+
+    @GetMapping("/regitrationConfirm")
+    public String confirmRegistration (WebRequest request, Model model, @RequestParam("token") String token) {
+        Locale locale = request.getLocale();
+
+        VerificationToken verificationToken = userService.getVerificationToken(token);
+        if (verificationToken == null) {
+            String message = messages.getMessage("auth.message.invalidToken", null, locale);
+            model.addAttribute("message", message);
+            return "redirect:/badUser.html?lang=" + locale.getLanguage();
+        }
+
+        User user = verificationToken.getUser();
+        Calendar cal = Calendar.getInstance();
+        if ((verificationToken.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
+            String messageValue = messages.getMessage("auth.message.expired", null, locale);
+            model.addAttribute("message", messageValue);
+            return "redirect:/badUser.html?lang=" + locale.getLanguage();
+        }
+
+        user.setEnabled(true);
+        userService.saveRegisteredUser(user);
+        return "redirect:/login.html?lang=" + request.getLocale().getLanguage();
+    }
+
 }
