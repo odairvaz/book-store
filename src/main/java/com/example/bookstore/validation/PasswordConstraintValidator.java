4435c50a454773350a5aa7a5a06bd53cd19dd2f2
Oda <odairvaz>
2023-11-18 18:29:43 +0100

    implement password validation rules, update unit tests

--- src/main/java/com/example/bookstore/validation/PasswordConstraintValidator.java
+++ src/main/java/com/example/bookstore/validation/PasswordConstraintValidator.java
@@ -9,28 +9,33 @@ import java.util.Arrays;
 
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
 
+
     @Override
-    public void initialize(ValidPassword constraintAnnotation) {}
+    public void initialize(ValidPassword constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
 
         final PasswordValidator validator = new PasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
-                new UppercaseCharacterRule(1),
-                new DigitCharacterRule(1),
-                new SpecialCharacterRule(1),
-                new NumericalSequenceRule(3,false),
-                new AlphabeticalSequenceRule(3,false),
-                new QwertySequenceRule(3,false),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new IllegalSequenceRule(EnglishSequenceData.Numerical, 3, false),
+                new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 3, false),
+                new IllegalSequenceRule(EnglishSequenceData.USQwerty, 3, false),
                 new WhitespaceRule()));
+
 
         final RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
             return true;
         }
         context.disableDefaultConstraintViolation();
-        context.buildConstraintViolationWithTemplate(Joiner.on(",").join(validator.getMessages(result))).addConstraintViolation();
+        context.buildConstraintViolationWithTemplate(Joiner.on(" ").join(validator.getMessages(result))).addConstraintViolation();
         return false;
     }
 }
