639bf3b55b22873ae0e61073fbf8c799cc80a536
Odair Vaz <vazodair@gmail.com>
2025-03-28 21:09:29 +0100

    fix script

--- src/main/resources/db/migration/V1__Add__UserID__Column__To__Review__Table.sql
+++ src/main/resources/db/migration/V1__Add__UserID__Column__To__Review__Table.sql
@@ -1,4 +1,9 @@
-ALTER TABLE review ADD COLUMN user_id BIGINT;
-ALTER TABLE review ADD CONSTRAINT fk_review_user FOREIGN KEY (user_id) REFERENCES user_account(id);
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'review' AND column_name = 'user_id') THEN
+            ALTER TABLE review ADD COLUMN user_id BIGINT;
+            ALTER TABLE review ADD CONSTRAINT fk_review_user FOREIGN KEY (user_id) REFERENCES user_account(id);
+        END IF;
+    END $$;
 
 UPDATE review SET user_id = (SELECT id FROM user_account WHERE id = 2) WHERE user_id IS NULL;
